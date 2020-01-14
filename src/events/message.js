d7e1fe540388ff62956874e75d7ce6f14db51c72
Shaun <59339807+d-shaun@users.noreply.github.com>
2020-01-14 18:13:34 +0545

    cache prefixes on message

--- src/events/message.js
+++ src/events/message.js
@@ -1,12 +1,30 @@
 module.exports = async (client, message) => {
-    if (!message.content.startsWith(client.prefix) || message.channel.type !== 'text' || message.author.bot) {
-        return
-    }
-    const args = message.content.slice(client.prefix.length).split(/ +/gi)
-    const commandName = args.shift().toLowerCase()
-    const command = client.commands.find(x => x.name === commandName || x.aliases.includes(commandName))
-    if (!command) {
-        return
-    }
-    await command.execute(client, message, args)
-}
+  if (!client.prefixes) {
+    await client.cachePrefixes();
+  }
+
+  const server_prefix = client.getCachedPrefix(message);
+
+  if (message.isMemberMentioned(client.user)) {
+    message.reply(
+      `the prefix for this server is \`\`${server_prefix}\`\`. Try \`\`${server_prefix}help\`\`.`
+    );
+  }
+
+  if (
+    !message.content.startsWith(server_prefix) ||
+    message.channel.type !== "text" ||
+    message.author.bot
+  ) {
+    return;
+  }
+  const args = message.content.slice(server_prefix.length).split(/ +/gi);
+  const commandName = args.shift().toLowerCase();
+  const command = client.commands.find(
+    x => x.name === commandName || x.aliases.includes(commandName)
+  );
+  if (!command) {
+    return;
+  }
+  await command.execute(client, message, args);
+};
