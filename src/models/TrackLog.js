2bb6c475e3d7dd7a4940980e7288f60522ce43be
Shaun <59339807+d-shaun@users.noreply.github.com>
2020-01-17 23:07:17 +0545

    new model to track track (heh) plays

---
+++ src/models/TrackLog.js
@@ -0,0 +1,11 @@
+
+
+module.exports = mongoose => {
+    return new mongoose.Schema({
+        name: String,
+        artistName: String,
+        userplaycount: Number,
+        userID: String,
+        timestamp: Number
+    })
+}
