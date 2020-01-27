3baffc120abbf5dede7834dbb88dd294f7c5a4cd
Shaun <59339807+d-shaun@users.noreply.github.com>
2020-01-27 05:47:39 -0500

    &crowns now works without @mentioning a user

--- src/commands/crowns.js
+++ src/commands/crowns.js
@@ -5,95 +5,69 @@ const path = require("path");
 const Pagination = require("discord-paginationembed");
 
 class CrownsCommand extends Command {
-	constructor() {
-		super({
-			name: "crowns",
-			description: "Displays crowns of an user.",
-			usage: ["crowns", "crowns <user>"],
-			aliases: ["cw"]
-		});
-	}
+  constructor() {
+    super({
+      name: "crowns",
+      description: "Displays crowns of an user.",
+      usage: ["crowns", "crowns <user>"],
+      aliases: ["cw"]
+    });
+  }
 
-	async run(client, message, args) {
-		const { check_permissions } = client.helpers;
+  async run(client, message, args) {
+    const { check_permissions, get_guild_user } = client.helpers;
 
-		const has_required_permissions = check_permissions(message);
-		if(!has_required_permissions) return;
+    const has_required_permissions = check_permissions(message);
+    if (!has_required_permissions) return;
 
-		const files = fs.readdirSync(path.join(__dirname, "crowns"));
-		const cmds = files.map(x => x.slice(0, x.length - 3));
-		let user;
-		if (args.length > 0) {
-			if (cmds.includes(args[0])) {
-				const command = require(`./crowns/${args[0]}`);
-				await command.run(client, message, args.slice(1));
-				return;
-			} else {
-				user = message.mentions.members.first();
-			}
-		} else {
-			user = message.member;
-		}
-		const crowns = await client.models.crowns.find({
-			guildID: message.guild.id,
-			userID: user.id
-		});
-		if (crowns.length > 0) {
-			let num = 0;
+    const files = fs.readdirSync(path.join(__dirname, "crowns"));
+    const cmds = files.map(x => x.slice(0, x.length - 3));
+    let user;
+    if (args.length > 0) {
+      user = message.mentions.members.first();
+      user = user ? user.user : await get_guild_user({ args, message });
+    } else {
+      user = message.member ? message.member.user : false;
+    }
 
-			const sorted_crowns = crowns.sort(
-				(a, b) => parseInt(b.artistPlays) - parseInt(a.artistPlays)
-			);
+    if (!user) {
+      await message.reply("user not found.");
+      return;
+    }
+    const crowns = await client.models.crowns.find({
+      guildID: message.guild.id,
+      userID: user.id
+    });
+    if (crowns.length > 0) {
+      let num = 0;
 
-			const FieldsEmbed = new Pagination.FieldsEmbed()
-				.setArray(sorted_crowns)
-				.setAuthorizedUsers([])
-				.setChannel(message.channel)
-				.setElementsPerPage(15)
-				.setPageIndicator(true)
-				.setDisabledNavigationEmojis(["DELETE"])
-				.formatField(
-					`Total: ${sorted_crowns.length} crowns`,
-					(el, i) =>
-						`${sorted_crowns.findIndex(
-							e => e.artistName == el.artistName
-						) + 1}. ${el.artistName} — **${
-							el.artistPlays
-						} play(s)**`
-				);
+      const sorted_crowns = crowns.sort(
+        (a, b) => parseInt(b.artistPlays) - parseInt(a.artistPlays)
+      );
 
-			FieldsEmbed.embed
-				.setColor(0x00ffff)
-				.setTitle(`Crowns of ${user.user.username} in ${message.guild.name}`)
-				.setThumbnail(user.user.avatarURL);
-			FieldsEmbed.build();
+      const FieldsEmbed = new Pagination.FieldsEmbed()
+        .setArray(sorted_crowns)
+        .setAuthorizedUsers([])
+        .setChannel(message.channel)
+        .setElementsPerPage(15)
+        .setPageIndicator(true)
+        .setDisabledNavigationEmojis(["DELETE"])
+        .formatField(
+          `Total: ${sorted_crowns.length} crowns`,
+          el =>
+            `${sorted_crowns.findIndex(e => e.artistName == el.artistName) +
+              1}. ${el.artistName} — **${el.artistPlays} play(s)**`
+        );
 
-			/*
-			const description =
-				crowns
-					.sort(
-						(a, b) =>
-							parseInt(b.artistPlays) - parseInt(a.artistPlays)
-					)
-					.slice(0, 10)
-					.map(
-						x =>
-							`${++num}. ${x.artistName} - **${
-								x.artistPlays
-							}** plays`
-					)
-					.join("\n") +
-				`\n\n${user.user.username} has **${crowns.length}** crowns in ${message.guild.name}.`;
-			const embed = new BotEmbed(message)
-				.setTitle(`Crowns of ${user.user.tag} in ${message.guild.name}`)
-				.setDescription(description)
-				.setThumbnail(user.user.avatarURL);
-			await message.channel.send(embed);
-			*/
-		} else {
-			await message.reply(`you don't have any crown in this server.`);
-		}
-	}
+      FieldsEmbed.embed
+        .setColor(0x00ffff)
+        .setTitle(`Crowns of ${user.username} in ${message.guild.name}`)
+        .setThumbnail(user.avatarURL);
+      await FieldsEmbed.build();
+    } else {
+      await message.reply(`you don't have any crown in this server.`);
+    }
+  }
 }
 
 module.exports = CrownsCommand;
