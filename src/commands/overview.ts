05966dee26bffdfa3e650df672bb3dd111588c81
Shaun <59339807+d-shaun@users.noreply.github.com>
2020-08-16 15:49:37 -0400

    accept and parse guild-user mention in args[]

--- src/commands/overview.ts
+++ src/commands/overview.ts
@@ -1,4 +1,4 @@
-import { Message, MessageEmbed } from "discord.js";
+import { Message, MessageEmbed, User } from "discord.js";
 import Command from "../classes/Command";
 import { Template } from "../classes/Template";
 import BotMessage from "../handlers/BotMessage";
@@ -21,19 +21,48 @@ class OverviewCommand extends Command {
 
   async run(client: CrownBot, message: Message, args: string[]) {
     const db = new DB(client.models);
-    const user = await db.fetch_user(message.author.id);
     const response = new BotMessage({ client, message, text: "", reply: true });
-
+    let discord_user: User;
+    let [last_item] = args.slice(-1);
+    let user;
+    if (last_item && last_item.startsWith("<@") && last_item.endsWith(">")) {
+      last_item = last_item.slice(2, -1);
+      if (last_item.startsWith("!")) {
+        last_item = last_item.slice(1);
+      }
+      const mention = message.guild?.members.cache.get(last_item);
+      if (!mention) {
+        response.text = "Couldn't find the mentioned user in this server.";
+        response.send();
+        return;
+      }
+      discord_user = mention.user;
+      user = await db.fetch_user(mention.user.id);
+      if (!user) {
+        response.text = "The mentioned user isn't logged into the bot.";
+        response.send();
+        return;
+      }
+      args.pop();
+    } else {
+      discord_user = message.author;
+      user = await db.fetch_user(message.author.id);
+    }
     if (!user) return;
-
     const lastfm_user = new LastFMUser({
-      discord_ID: message.author.id,
+      discord_ID: user.user_ID,
       username: user.username,
+    });
+    const author_db_user = await db.fetch_user(message.author.id);
+    if (!author_db_user) return;
+    const author_user = new LastFMUser({
+      username: author_db_user.username,
+      discord_ID: author_db_user.user_ID,
     });
 
     let artist_name;
     if (args.length === 0) {
-      const now_playing = await lastfm_user.get_nowplaying(client, message);
+      const now_playing = await author_user.get_nowplaying(client, message);
       if (!now_playing) return;
       artist_name = now_playing.artist["#text"];
     } else {
@@ -61,7 +90,7 @@ class OverviewCommand extends Command {
     const artist: ArtistInterface = data.artist;
     if (!artist.stats.userplaycount) return;
     if (parseInt(artist.stats.userplaycount) <= 0) {
-      response.text = `You haven't scrobbled \`${artist.name}\`.`;
+      response.text = `${discord_user.username} hasn't scrobbled \`${artist.name}\`.`;
       await response.send();
       return;
     }
@@ -81,7 +110,7 @@ class OverviewCommand extends Command {
       return `${i + 1}. **${track.name}** (${track.plays})`;
     });
     const embed = new MessageEmbed()
-      .setTitle(`\`${artist.name}\` overview for ${message.author.username}`)
+      .setTitle(`\`${artist.name}\` overview for ${discord_user.username}`)
       .addField(
         "Scrobbles",
         `**${artist.stats.userplaycount}** plays—**${albums.length}** albums · **${tracks.length}** tracks`,
