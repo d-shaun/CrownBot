9d4aa3c1a33a07317b0789dc1fac1ff6bb9b3e01
d-shaun <d-shaun@users.noreply.github.com>
2020-10-06 12:17:40 -0400

    + random strings generator

---
+++ src/misc/generate_random_strings.ts
@@ -0,0 +1,11 @@
+export default function generate_random_strings(length = 4): string {
+  // https://stackoverflow.com/a/1349426
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
