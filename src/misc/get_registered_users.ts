6b5fb51f1e74a792b4d9471f47296aa3a0136362
d-shaun <d-shaun@users.noreply.github.com>
2020-11-02 09:26:14 -0500

    refactoring core components to not use the privileged intents

--- src/misc/get_registered_users.ts
+++ src/misc/get_registered_users.ts
@@ -1,11 +1,13 @@
-import { Message } from "discord.js";
+import { GuildMember, Message } from "discord.js";
 import CrownBot from "../handlers/CrownBot";
 import { BanInterface } from "../stable/models/Bans";
 import { UserInterface } from "../stable/models/Users";
+
 interface UserFetchInterface {
-  users: UserInterface[];
-  banned_users: BanInterface[];
-  banned_ids: string[];
+  users: {
+    discord: GuildMember;
+    database: UserInterface;
+  }[];
 }
 
 export default async function get_registered_users(
@@ -14,24 +16,42 @@ export default async function get_registered_users(
 ): Promise<UserFetchInterface | undefined> {
   if (!message.guild) return undefined;
 
-  const user_ids = message.guild.members.cache.map((member) => member.id);
-  if (!user_ids) throw "Failed to fetch guild members.";
-  const all_users: UserInterface[] = await client.models.serverusers
+  const database_users: UserInterface[] = await client.models.serverusers
     .find({
       guildID: message.guild.id,
-      userID: {
-        $in: user_ids,
-      },
     })
     .lean();
 
   const banned_users: BanInterface[] = await client.models.bans
     .find({
-      userID: { $in: user_ids },
       guildID: { $in: [message.guild.id, "any"] },
     })
     .lean();
   const banned_ids = banned_users.map((user) => user.userID);
-  const users = all_users.filter((user) => !banned_ids.includes(user.userID));
-  return { users, banned_users, banned_ids };
+
+  const relevant_users = [
+    ...(
+      await message.guild.members.fetch({
+        user: database_users.map((user) => user.userID),
+      })
+    ).entries(),
+  ].filter((user) => !banned_ids.includes(user[0]));
+
+  const relevant_users_id = relevant_users.map((user) => user[0]);
+
+  const users = database_users
+    .filter((database_user) => relevant_users_id.includes(database_user.userID))
+    .map((database_user) => {
+      const t_2 = relevant_users.find(
+        ([user_id]) => user_id === database_user.userID
+      );
+      if (!t_2) throw "not possible";
+      const discord = t_2[1];
+      return {
+        discord,
+        database: database_user,
+      };
+    });
+
+  return { users };
 }
--- src/misc/search_user.ts
+++ src/misc/search_user.ts
@@ -1,17 +1,14 @@
 import { Message, User } from "discord.js";
-export default function search_user(
+export default async function search_user(
   message: Message,
   args: string[]
-): User | undefined {
+): Promise<User | undefined> {
   if (args.length === 0) {
     return undefined;
   }
-  const username = args.join(" ").trim();
-  let user = message.guild?.members.cache.find((member) => {
-    return member.user.username
-      .toLowerCase()
-      .startsWith(username.toLowerCase());
-  });
-
+  const username = args.join(" ").trim().toLowerCase();
+  const user = (
+    await message.guild?.members.fetch({ query: username })
+  )?.first(); // find similar username
   return user ? user.user : undefined;
 }
