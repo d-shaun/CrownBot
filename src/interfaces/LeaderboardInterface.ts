4a69e4abbea49fe0bce95092c067caa47400426b
d-shaun <d-shaun@users.noreply.github.com>
2020-11-26 10:01:42 -0500

    LeaderboardInterface: + album_name

--- src/interfaces/LeaderboardInterface.ts
+++ src/interfaces/LeaderboardInterface.ts
@@ -1,6 +1,7 @@
 export interface LeaderboardInterface {
   artist_name: string;
   track_name?: string;
+  album_name?: string;
   discord_username: string;
   lastfm_username: string;
   userplaycount: string;
