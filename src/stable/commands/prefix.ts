23a7c639ad3e93abcb6cac25d6bb9b9d35fe7ba6
d-shaun <d-shaun@users.noreply.github.com>
2020-12-11 11:08:40 -0500

    changed &prefix to use the prefix cache handler

--- src/stable/commands/prefix.ts
+++ src/stable/commands/prefix.ts
@@ -1,5 +1,7 @@
 import Command, { GuildMessage } from "../../classes/Command";
+import BotMessage from "../../handlers/BotMessage";
 import CrownBot from "../../handlers/CrownBot";
+import cb from "../../misc/codeblock";
 
 class PrefixCommand extends Command {
   constructor() {
@@ -14,52 +16,44 @@ class PrefixCommand extends Command {
   }
 
   async run(client: CrownBot, message: GuildMessage, args: string[]) {
-    /* WIP */
-    /*  
-    
-    
-    
-    
-    */
-    // const server_prefix = client.cache.prefix.get(message.guild);
-    // const response = new BotMessage({ client, message, text: "", reply: true });
-    // if (args.length === 0) {
-    //   response.text =
-    //     `The prefix for this server is \`${server_prefix}\`; ` +
-    //     `execute ${cb("prefix <new_prefix>", server_prefix)} to change it.`;
-    //   await response.send();
-    //   return;
-    // }
-    // if (!message.member?.hasPermission("MANAGE_GUILD")) {
-    //   response.text =
-    //     "You do not have the permission (``MANAGE_GUILD``) to execute this command.";
-    //   await response.send();
-    //   return;
-    // }
-    // const prefix = args.join(" ");
-    // if (!prefix.match(/^\S{1,4}$/g)) {
-    //   response.text = "Invalid prefix.";
-    //   await response.send();
-    //   return;
-    // }
-    // await client.models.prefixes.findOneAndUpdate(
-    //   {
-    //     guildID: message.guild.id,
-    //   },
-    //   {
-    //     guildID: message.guild.id,
-    //     prefix,
-    //     guildName: message.guild.name,
-    //   },
-    //   {
-    //     upsert: true,
-    //     // @ts-ignore
-    //     useFindAndModify: false,
-    //   }
-    // );
-    // client.prefixes = undefined;
-    // response.text = `The prefix for this server is now set to ${cb(prefix)}.`;
-    // response.send();
+    const server_prefix = client.cache.prefix.get(message.guild);
+    const response = new BotMessage({ client, message, text: "", reply: true });
+    if (args.length === 0) {
+      response.text =
+        `The prefix for this server is \`${server_prefix}\`; ` +
+        `execute ${cb("prefix <new_prefix>", server_prefix)} to change it.`;
+      await response.send();
+      return;
+    }
+    if (!message.member?.hasPermission("MANAGE_GUILD")) {
+      response.text =
+        "You do not have the permission (``MANAGE_GUILD``) to execute this command.";
+      await response.send();
+      return;
+    }
+    const prefix = args.join(" ");
+    if (!prefix.match(/^\S{1,4}$/g)) {
+      response.text = "Invalid prefix.";
+      await response.send();
+      return;
+    }
+    await client.models.prefixes.findOneAndUpdate(
+      {
+        guildID: message.guild.id,
+      },
+      {
+        guildID: message.guild.id,
+        prefix,
+        guildName: message.guild.name,
+      },
+      {
+        upsert: true,
+        useFindAndModify: false,
+      }
+    );
+    client.cache.prefix.set(prefix, message.guild);
+    response.text = `The prefix for this server is now set to ${cb(prefix)}.`;
+    response.send();
   }
 }
 
