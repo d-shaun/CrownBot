6b737042ef1f8cdfcc9a2224098a5c01e491775e
d-shaun <d-shaun@users.noreply.github.com>
2020-12-26 12:53:30 -0500

    `search_user`: hotfix for searching by username

--- src/misc/search_user.ts
+++ src/misc/search_user.ts
@@ -8,9 +8,14 @@ export default async function search_user(
     return undefined;
   }
   const username = args.join(" ").trim().toLowerCase();
-  let user = <GuildMember | undefined>(
-    await message.guild.members.fetch({ user: username, force: true })
-  );
+  let user: GuildMember | undefined;
+  // workaround to support resolving user ID and searching username
+  try {
+    user = await message.guild.members.fetch({ user: username, force: true });
+  } catch (_) {
+    user = undefined;
+  }
+
   if (!user) {
     user = (
       await message.guild.members.fetch({ query: username, limit: 1 })
