08a0680e54a67286ae639d2fe915815db55d0f96
d-shaun <d-shaun@users.noreply.github.com>
2021-03-09 09:06:07 -0500

    &lyrics: don't route through Last.fm

--- src/beta/commands/lyrics.ts
+++ src/beta/commands/lyrics.ts
@@ -4,9 +4,7 @@ import Command, { GuildMessage } from "../../classes/Command";
 import BotMessage from "../../handlers/BotMessage";
 import CrownBot from "../../handlers/CrownBot";
 import DB from "../../handlers/DB";
-import Track from "../../handlers/LastFM_components/Track";
 import User from "../../handlers/LastFM_components/User";
-import cb from "../../misc/codeblock";
 import esm from "../../misc/escapemarkdown";
 import { LyricsLogInterface } from "../../stable/models/LyricsLog";
 
@@ -27,7 +25,6 @@ class LyricsCommand extends Command {
     if (!client.genius_api) {
       throw "You must set the `GENIUS_API` in the environment variable for this command to work.";
     }
-    const server_prefix = client.cache.prefix.get(message.guild);
     const db = new DB(client.models);
     const user = await db.fetch_user(message.guild.id, message.author.id);
     if (!user) return;
@@ -37,60 +34,20 @@ class LyricsCommand extends Command {
       username: user.username,
     });
 
-    let artist_name;
-    let track_name;
+    let search_query;
     if (args.length === 0) {
       const now_playing = await lastfm_user.get_nowplaying(client, message);
       if (!now_playing) return;
-      artist_name = now_playing.artist["#text"];
-      track_name = now_playing.name;
-    } else {
-      const str = args.join(" ");
-      const str_array = str.split("||");
-      if (str_array.length !== 2) {
-        const query = await new Track({
-          name: str_array.join().trim(),
-          limit: 1,
-        }).search();
-        if (query.lastfm_errorcode || !query.success) {
-          response.error("lastfm_error", query.lastfm_errormessage);
-          return;
-        }
+      const {
+        name: track_name,
+        artist: { "#text": artist_name },
+      } = now_playing;
 
-        const track = query.data.results.trackmatches.track.shift();
-
-        if (!track) {
-          response.text = `Couldn't find the track; try providing artist name—see ${cb(
-            "help lyrics",
-            server_prefix
-          )}.`;
-          await response.send();
-          return;
-        }
-        track_name = track.name;
-        artist_name = track.artist;
-      } else {
-        track_name = str_array[0].trim();
-        artist_name = str_array[1].trim();
-      }
-    }
-
-    const query = await new Track({
-      name: track_name,
-      artist_name,
-      username: user.username,
-    }).user_get_info();
-    if (query.lastfm_errorcode || !query.success) {
-      response.error("lastfm_error", query.lastfm_errormessage);
-      return;
-    }
-
-    const track = query.data.track;
+      search_query = `${track_name} ${artist_name}`;
+    } else search_query = args.join(" ");
 
     const lyricist = new Lyricist(client.genius_api);
-    const song = (
-      await lyricist.search(`${track.name} ${track.artist.name}`)
-    )[0];
+    const song = (await lyricist.search(search_query))[0];
     if (!song) {
       response.text = "Couldn't find the song.";
       await response.send();
