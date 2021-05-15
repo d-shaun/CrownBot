f9f5067b6bdba60c1525dfba7961f5d301cabf27
d-shaun <d-shaun@users.noreply.github.com>
2021-05-15 11:26:48 -0400

    esm: option to truncate str after "escaping"

--- src/misc/escapemarkdown.ts
+++ src/misc/escapemarkdown.ts
@@ -1,8 +1,12 @@
+import truncate_str from "./truncate";
+
 /**
  * Escapes Discord's markdown characters.
  * @param text
+ * @param truncate Truncates string, *after* "escaping", to 25 chars if set to true.
+ * @returns
  */
-export default function esm(text: string) {
+export default function esm(text: string, truncate = false) {
   // https://stackoverflow.com/questions/39542872/escaping-discord-subset-of-markdown
 
   /* replace backticks (`) with single-quotes (') */
@@ -11,5 +15,5 @@ export default function esm(text: string) {
   // .replace(/\\(\*|_|`|~|\\)/g, "$1"); // unescape any "backslashed" character
   const escaped = unescaped.replace(/(\*|_|`|~|\\)/g, "\\$1").trim(); // escape *, _, `, ~, \
 
-  return escaped;
+  return truncate ? truncate_str(escaped, 25) : escaped;
 }
