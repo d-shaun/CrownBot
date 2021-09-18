83735c31cce39cb0ad42f097b1f686eaf72ce52f
d-shaun <d-shaun@users.noreply.github.com>
2021-09-18 02:25:38 -0400

    interaction -> interactionCreate (placeholder)

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,10 +1,5 @@
 import CrownBot from "../handlers/CrownBot";
 import { help_navigate } from "../stable/commands/help";
 export default async (bot: CrownBot, menu: any) => {
-  console.log(menu);
-  // switch (menu.id) {
-  //   case "help_menu" + client.buttons_version:
-  //     help_navigate(client, menu);
-  //     break;
-  // }
+  // placeholder
 };
