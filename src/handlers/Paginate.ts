0fabca995d85a2910948f5ebe53dc982da9e737c
d-shaun <d-shaun@users.noreply.github.com>
2021-09-18 07:48:29 -0400

    WIP: pagination handler

---
+++ src/handlers/Paginate.ts
@@ -0,0 +1,77 @@
+import { Message, MessageButton, MessageEmbed } from "discord.js";
+const paginationEmbed = require("discordjs-button-pagination");
+
+export default class Paginate {
+  message: Message;
+  embed: MessageEmbed;
+  list: string[];
+  elements: number;
+  numbering: boolean;
+
+  constructor(
+    message: Message,
+    embed: MessageEmbed,
+    list: string[],
+    elements = 15,
+    numbering = true
+  ) {
+    this.message = message;
+    this.embed = embed;
+    this.list = list;
+    this.elements = elements;
+    this.numbering = numbering;
+  }
+
+  gen_list(elems: string[]) {
+    return elems.map((e) => `\n${e}\n`);
+  }
+
+  chunk(arr: string[], len: number) {
+    const chunks = [],
+      n = arr.length;
+    let i = 0;
+    while (i < n) {
+      chunks.push(arr.slice(i, (i += len)));
+    }
+
+    return chunks;
+  }
+
+  async send() {
+    const embeds: MessageEmbed[] = [];
+
+    if (this.numbering) {
+      this.list = this.list.map((item, i) => `${i + 1}. ${item}`);
+    }
+    const chunks = this.chunk(this.list, this.elements);
+
+    chunks.forEach((chunk, i) => {
+      embeds[i] = Object.create(this.embed);
+      let new_text = chunk.join("\n");
+      if (this.embed.description) {
+        new_text = `${this.embed.description}\n` + new_text;
+      }
+      embeds[i].description = new_text + "\n";
+      if (this.embed.footer?.text && chunks.length >= 2) {
+        embeds[i].description += "\n" + this.embed.footer.text;
+      }
+    });
+
+    const button1 = new MessageButton()
+      .setCustomId("previousbtn")
+      .setLabel("Previous")
+      .setStyle("SUCCESS");
+
+    const button2 = new MessageButton()
+      .setCustomId("nextbtn")
+      .setLabel("Next")
+      .setStyle("SUCCESS");
+    const buttonList = [button1, button2];
+
+    if (chunks.length >= 2) {
+      return paginationEmbed(this.message, embeds, buttonList);
+    } else {
+      return this.message.channel.send({ embeds: [embeds[0]] });
+    }
+  }
+}
