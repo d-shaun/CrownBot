d247ce73d32817300d432fbc60e9c588cea2cafc
d-shaun <d-shaun@users.noreply.github.com>
2021-09-29 02:03:48 -0400

    WIP &trending: added error messages and Paginate

--- src/stable/commands/trending.ts
+++ src/stable/commands/trending.ts
@@ -1,8 +1,12 @@
+import { MessageEmbed } from "discord.js";
 import Command, { GuildMessage } from "../../classes/Command";
+import BotMessage from "../../handlers/BotMessage";
 import CrownBot from "../../handlers/CrownBot";
 import DB from "../../handlers/DB";
 import Artist from "../../handlers/LastFM_components/Artist";
 import User from "../../handlers/LastFM_components/User";
+import Paginate from "../../handlers/Paginate";
+import esm from "../../misc/escapemarkdown";
 
 class TrendingCommand extends Command {
   constructor() {
@@ -20,7 +24,7 @@ class TrendingCommand extends Command {
   async run(bot: CrownBot, message: GuildMessage, args: string[]) {
     const db = new DB(bot.models);
     const user = await db.fetch_user(message.guild.id, message.author.id);
-    // const response = new BotMessage({ bot, message, reply: true });
+    const response = new BotMessage({ bot, message, reply: true });
 
     if (!user) return;
     const lastfm_user = new User({
@@ -37,13 +41,31 @@ class TrendingCommand extends Command {
     }
 
     const artist = new Artist({ name: artist_name });
+    const query = await artist.get_info();
+
+    if (query.lastfm_errorcode || !query.success) {
+      response.error("lastfm_error", query.lastfm_errormessage);
+      return;
+    }
+    artist.name = query.data.artist.name;
+
     const trending = await artist.get_trending();
     if (!trending) {
       // TODD: error message here
       return;
     }
 
-    console.log(trending);
+    const embed = new MessageEmbed().setTitle(
+      `${esm(artist.name)}'s trending tracks this week`
+    );
+
+    const data_list = trending.map((elem) => {
+      return `**${elem.name}** — **${elem.listeners}** listeners`;
+    });
+
+    const paginate = new Paginate(message, embed, data_list);
+
+    await paginate.send();
   }
 }
 
