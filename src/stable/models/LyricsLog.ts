0193b09d208f88918e268a14c0fc80b705e2017a
d-shaun <d-shaun@users.noreply.github.com>
2021-10-09 14:40:57 -0400

    LyricsLog: rm id, added 'permanent' field

--- src/stable/models/LyricsLog.ts
+++ src/stable/models/LyricsLog.ts
@@ -1,18 +1,18 @@
 import { Mongoose } from "mongoose";
 
 export interface LyricsLogInterface {
-  id: number;
   track_name: string;
   artist_name: string;
   lyrics: string;
   timestamp: number;
+  permanent?: boolean;
 }
 export default (mongoose: Mongoose) => {
   return new mongoose.Schema({
-    id: Number,
     track_name: String,
     artist_name: String,
     lyrics: String,
     timestamp: Number,
+    permanent: Boolean,
   });
 };
