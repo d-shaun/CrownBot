687571ac57b436560106612baa5fe80822b1ba24
d-shaun <d-shaun@users.noreply.github.com>
2021-10-23 13:40:43 -0400

    &purgelyrics: use lfm to find lyrics; ignore permanent

--- src/beta/commands/purgelyrics.ts
+++ src/beta/commands/purgelyrics.ts
@@ -1,9 +1,9 @@
-import Lyricist from "lyricist";
 import Command, { GuildMessage } from "../../classes/Command";
 import { Template } from "../../classes/Template";
 import BotMessage from "../../handlers/BotMessage";
 import CrownBot from "../../handlers/CrownBot";
 import DB from "../../handlers/DB";
+import Track from "../../handlers/LastFM_components/Track";
 import User from "../../handlers/LastFM_components/User";
 import esm from "../../misc/escapemarkdown";
 import { LyricsLogInterface } from "../../stable/models/LyricsLog";
@@ -34,48 +34,77 @@ class PurgeLyricsCommand extends Command {
       username: user.username,
     });
 
-    let search_query;
+    let artist_name;
+    let track_name;
     if (args.length === 0) {
       const now_playing = await lastfm_user.get_nowplaying(bot, message);
       if (!now_playing) return;
-      const {
-        name: track_name,
-        artist: { "#text": artist_name },
-      } = now_playing;
+      artist_name = now_playing.artist["#text"];
+      track_name = now_playing.name;
+    } else {
+      const str = args.join(" ");
+      const str_array = str.split("||");
+      if (str_array.length !== 2) {
+        const query = await new Track({
+          name: str_array.join().trim(),
+          limit: 1,
+        }).search();
+        if (query.lastfm_errorcode || !query.success) {
+          response.error("lastfm_error", query.lastfm_errormessage);
+          return;
+        }
+        const track = query.data.results.trackmatches.track.shift();
 
-      search_query = `${track_name} ${artist_name}`;
-    } else search_query = args.join(" ");
-
-    const lyricist = new Lyricist(bot.genius_api);
-    const song = (await lyricist.search(search_query))[0];
-    if (!song) {
-      response.text = "Couldn't find the song.";
-      await response.send();
-      return;
+        if (!track) {
+          response.text = `Couldn't find the track`;
+          await response.send();
+          return;
+        }
+        track_name = track.name;
+        artist_name = track.artist;
+      } else {
+        track_name = str_array[0].trim();
+        artist_name = str_array[1].trim();
+      }
     }
 
+    const query = await new Track({
+      name: track_name,
+      artist_name,
+    }).get_info();
+    if (query.lastfm_errorcode || !query.success) {
+      response.error("lastfm_error", query.lastfm_errormessage);
+      return;
+    }
+    const track = query.data.track;
+
     const db_entry: LyricsLogInterface = await bot.models.lyricslog.findOne({
-      id: song.id,
+      track_name: track.name,
+      artist_name: track.artist.name,
+      permanent: {
+        $ne: true,
+      },
     });
     if (db_entry) {
       const db_response = await bot.models.lyricslog.deleteOne({
-        id: song.id,
+        track_name: track.name,
+        artist_name: track.artist.name,
         permanent: {
           $ne: true,
         },
       });
       if (db_response) {
         response.text = `Successfully purged the cached lyrics for **${esm(
-          song.title
-        )}** by **${esm(song.primary_artist.name)}**.`;
+          track.name
+        )}** by **${esm(track.artist.name)}**.`;
       } else {
         response.text = new Template(bot, message).get("exception");
       }
       await response.send();
     } else {
       response.text = `Couldn't find cached lyrics for **${esm(
-        song.title
-      )}** by **${esm(song.primary_artist.name)}**.`;
+        track.name
+      )}** by **${esm(track.artist.name)}**.`;
       await response.send();
     }
   }
