6548b41eaafe4feafd2f2bb40e44dee1d8150c70
d-shaun <d-shaun@users.noreply.github.com>
2022-09-27 10:18:20 -0400

    fx error on new configs

--- src/handlers/cache_components/Config.ts
+++ src/handlers/cache_components/Config.ts
@@ -54,6 +54,11 @@ export class Config implements CacheComponent {
 
       return config;
     });
+
+    if (!changed) {
+      this.#configs.push(new_config);
+      changed = true;
+    }
     return changed;
   }
 }
