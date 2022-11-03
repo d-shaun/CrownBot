5a08bc3753cc0a86a8bcdf46a23c3364d882a287
d-shaun <d-shaun@users.noreply.github.com>
2022-11-04 04:56:40 +0545

    + parse_spotify_presence

---
+++ src/misc/parse_spotify_presence.ts
@@ -0,0 +1,25 @@
+import { APIInteractionGuildMember, GuildMember } from "discord.js";
+
+export default function parse_spotify(
+  member: GuildMember | APIInteractionGuildMember | null
+) {
+  let artist_name = null,
+    album_name = null,
+    track_name = null;
+
+  if (member instanceof GuildMember) {
+    const spotify_presence = member.presence?.activities.find(
+      (act) => act.name === "Spotify"
+    );
+    if (spotify_presence) {
+      artist_name = spotify_presence.state?.split("; ")[0] || null;
+      album_name = spotify_presence.assets?.largeText || null;
+      track_name = spotify_presence.details || null;
+    }
+  }
+  return {
+    artist_name,
+    album_name,
+    track_name,
+  };
+}
