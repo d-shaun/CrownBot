11b4f92d3425ebc562b5c551c4486fe9507e3704
d-shaun <d-shaun@users.noreply.github.com>
2022-11-04 12:47:32 +0545

    + more available props

--- src/misc/parse_spotify_presence.ts
+++ src/misc/parse_spotify_presence.ts
@@ -5,7 +5,8 @@ export default function parse_spotify(
 ) {
   let artist_name = null,
     album_name = null,
-    track_name = null;
+    track_name = null,
+    createdTimeStamp = null;
 
   if (member instanceof GuildMember) {
     const spotify_presence = member.presence?.activities.find(
@@ -15,11 +16,13 @@ export default function parse_spotify(
       artist_name = spotify_presence.state?.split("; ")[0] || null;
       album_name = spotify_presence.assets?.largeText || null;
       track_name = spotify_presence.details || null;
+      createdTimeStamp = spotify_presence.createdTimestamp;
     }
   }
   return {
     artist_name,
     album_name,
     track_name,
+    createdTimeStamp,
   };
 }
