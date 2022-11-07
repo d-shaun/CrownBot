ce47f1b7efb6bce65a4e6d05f604024530658b97
d-shaun <d-shaun@users.noreply.github.com>
2022-11-07 01:44:31 -0500

    removed prefix from init, cache and model :(

--- src/handlers/Cache.ts
+++ src/handlers/Cache.ts
@@ -1,18 +1,15 @@
 import { Artists } from "./cache_components/Artists";
 import { Config } from "./cache_components/Config";
-import { Prefix } from "./cache_components/Prefix";
 import CrownBot from "./CrownBot";
 
 export default class CacheHandler {
   #bot: CrownBot;
   config: Config;
-  prefix: Prefix;
   artists: Artists;
 
   constructor(client: CrownBot) {
     this.#bot = client;
     this.config = new Config(this.#bot);
-    this.prefix = new Prefix(this.#bot);
     this.artists = new Artists(this.#bot);
   }
 }
