b7df401b4fff0c1f3a7a1f0c8b67458cf5814641
d-shaun <d-shaun@users.noreply.github.com>
2022-12-11 12:27:06 -0500

    expanded Collectors

--- src/handlers/cache_components/Collectors.ts
+++ src/handlers/cache_components/Collectors.ts
@@ -2,15 +2,21 @@ import { ButtonInteraction, InteractionCollector } from "discord.js";
 import { CacheComponent } from "../../interfaces/CacheComponentInterface";
 
 export class Collectors implements CacheComponent {
-  entries: InteractionCollector<any>[] = [];
+  #entries: InteractionCollector<any>[] = [];
 
   async init() {
     // placeholder, not needed
     return true;
   }
 
+  get() {
+    return this.#entries;
+  }
+
   add(collector: InteractionCollector<ButtonInteraction>) {
-    if (this.entries.length > 100) this.entries.length = 90;
-    this.entries.unshift(collector);
+    // remove ended entries
+    this.#entries = this.#entries.filter((e) => !e.ended);
+    if (this.#entries.length > 100) this.#entries.length = 90;
+    this.#entries.unshift(collector);
   }
 }
