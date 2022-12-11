cc4c7622a235604f2ec44682068526e73b21147c
d-shaun <d-shaun@users.noreply.github.com>
2022-12-11 11:22:05 -0500

    made default Cache methods optional

--- src/interfaces/CacheComponentInterface.ts
+++ src/interfaces/CacheComponentInterface.ts
@@ -1,6 +1,6 @@
 export interface CacheComponent {
-  init: () => Promise<boolean>;
-  check: () => Promise<boolean>;
-  get: (...args: string[]) => any | Promise<any>;
-  set: (...args: any[]) => boolean | Promise<boolean>;
+  init?: () => Promise<boolean>;
+  check?: () => Promise<boolean>;
+  get?: (...args: string[]) => any | Promise<any>;
+  set?: (...args: any[]) => boolean | Promise<boolean>;
 }
